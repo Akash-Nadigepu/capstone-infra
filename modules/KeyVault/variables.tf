c2296fe4b784cb5db9ed927c79378c4bf2f19926
devuser08 <devuser08@refabs365.com>
2025-06-22 17:58:06 +0000

    Added variables.tf

---
+++ modules/KeyVault/variables.tf
@@ -0,0 +1,7 @@
+# modules/KeyVault/variables.tf
+variable "key_vault_name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "tenant_id" {}
+variable "object_id" {}
+variable "environment" {}
