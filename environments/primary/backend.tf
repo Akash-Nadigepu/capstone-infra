497f183f21e33f2be4bcddb3c0c9b730698869fa
devuser08 <devuser08@refabs365.com>
2025-06-22 13:17:04 +0000

    Updated backend.tf

--- environments/primary/backend.tf
+++ environments/primary/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "capstonetfstate"     
-    container_name       = "tfstate"
-    key                  = "primary.terraform.tfstate"
-  }
-}
+// terraform {
+//   backend "azurerm" {
+//     resource_group_name  = "tfstate-rg"
+//     storage_account_name = "capstonetfstate"     
+//     container_name       = "tfstate"
+//     key                  = "primary.terraform.tfstate"
+//   }
+// }
