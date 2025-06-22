53b437dd8d3db463433e3850b051a35b508763ce
devuser08 <devuser08@refabs365.com>
2025-06-22 19:52:18 +0000

    Updated main.tf

--- environments/primary/main.tf
+++ environments/primary/main.tf
@@ -5,13 +5,8 @@ terraform {
       version = "~> 3.0"
     }
   }
+
   required_version = ">= 1.3.0"
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-backend"
-    storage_account_name = "tfstateakashdemo"
-    container_name       = "tfstate"
-    key                  = "primary.terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
@@ -24,16 +19,10 @@ resource "azurerm_resource_group" "primary" {
   location = "centralindia"
 }
 
-resource "random_string" "suffix" {
-  length  = 4
-  upper   = false
-  special = false
-}
-
 module "virtual_network" {
   source              = "../../modules/VirtualNetwork"
-  location            = azurerm_resource_group.primary.location
-  resource_group_name = azurerm_resource_group.primary.name
+  location            = "centralindia"
+  resource_group_name = "rg-primary"
   vnet_name           = "primary-vnet"
   address_space       = "10.10.0.0/16"
 
@@ -44,20 +33,25 @@ module "virtual_network" {
 
   agw_subnet_prefix = "10.10.3.0/24"
   sql_subnet_prefix = "10.10.4.0/24"
-
+  
   depends_on = [azurerm_resource_group.primary]
+}
+
+resource "random_string" "suffix" {
+  length  = 4
+  upper   = false
+  special = false
 }
 
 module "key_vault" {
   source              = "../../modules/KeyVault"
-  key_vault_name      = "kv-primary-${random_string.suffix.result}"
-  resource_group_name = azurerm_resource_group.primary.name
-  location            = azurerm_resource_group.primary.location
+  key_vault_name = "kv-primary-${random_string.suffix.result}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
   tenant_id           = var.tenant_id
   object_id           = var.object_id
-  environment         = "dev"
+  environment         = "dev" 
 }
-
 module "aks" {
   source              = "../../modules/AKS"
   aks_name            = "aks-primary"
