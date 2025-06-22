057fdbf5fe97be819198a9448e6f99920004607d
devuser08 <devuser08@refabs365.com>
2025-06-22 19:40:13 +0000

    Updated output.tf

--- modules/AKS/output.tf
+++ modules/AKS/output.tf
@@ -2,3 +2,10 @@ output "kube_config" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
+output "nodepool1_subnet_id" {
+  value = azurerm_subnet.nodepool1.id
+}
+
+output "nodepool2_subnet_id" {
+  value = azurerm_subnet.nodepool2.id
+}
