7893377869b30ef1237cb278f50368a9dfdcb581
devuser08 <devuser08@refabs365.com>
2025-06-22 13:12:11 +0000

    Updated main.tf

--- environments/primary/main.tf
+++ environments/primary/main.tf
@@ -1,14 +1,5 @@
 provider "azurerm" {
   features = {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-
-  use_cli                    = false
-  use_msi                    = false
-  skip_provider_registration = true
 }
 
 
