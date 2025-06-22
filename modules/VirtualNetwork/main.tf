1033f252e7a4a449ff2c33e92000fd36c32ff2fe
devuser08 <devuser08@refabs365.com>
2025-06-22 08:09:19 +0000

    Updated main.tf

---
+++ modules/VirtualNetwork/main.tf
@@ -0,0 +1,29 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = [var.address_space]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "aks" {
+  for_each = var.aks_subnets
+
+  name                 = "subnet-${each.key}"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [each.value]
+}
+
+resource "azurerm_subnet" "agw" {
+  name                 = "subnet-agw"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.agw_subnet_prefix]
+}
+
+resource "azurerm_subnet" "sql" {
+  name                 = "subnet-sql"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.sql_subnet_prefix]
+}
