aa27f4143f7335c88eb01c22a565b4c20c33791e
devuser08 <devuser08@refabs365.com>
2025-06-22 17:57:41 +0000

    Added main.tf

---
+++ modules/KeyVault/main.tf
@@ -0,0 +1,21 @@
+# modules/KeyVault/main.tf
+resource "azurerm_key_vault" "this" {
+  name                        = var.key_vault_name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  soft_delete_enabled         = true
+  purge_protection_enabled    = true
+  public_network_access_enabled = false
+
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = var.object_id # This should be your SP/App ID or User
+    secret_permissions = ["get", "list", "set", "delete"]
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
