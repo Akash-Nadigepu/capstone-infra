5308572e89a2d7f7a4cc9fe4771e788b7fe6cbe9
devuser08 <devuser08@refabs365.com>
2025-06-23 05:08:37 +0000

    Updated output.tf

--- modules/AKS/output.tf
+++ modules/AKS/output.tf
@@ -1,4 +1,10 @@
+# modules/AKS/outputs.tf
+
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
+}
+
+output "kubelet_identity_object_id" {
+  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
