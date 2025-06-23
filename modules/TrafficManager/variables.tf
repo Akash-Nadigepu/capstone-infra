bd93f2aec84457ce13b25394b94f7945346e497c
devuser08 <devuser08@refabs365.com>
2025-06-23 09:55:17 +0000

    Added variables.tf

---
+++ modules/TrafficManager/variables.tf
@@ -0,0 +1,8 @@
+variable "profile_name" {}
+variable "dns_name" {}
+variable "resource_group_name" {}
+variable "primary_endpoint" {}
+variable "secondary_endpoint" {}
+variable "environment" {
+  default = "prod"
+}
