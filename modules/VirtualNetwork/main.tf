7214c4669157e299a19b7c7025b850fecacb532d
devuser08 <devuser08@refabs365.com>
2025-06-23 05:45:11 +0000

    Updated main.tf

--- modules/VirtualNetwork/main.tf
+++ modules/VirtualNetwork/main.tf
@@ -22,6 +22,8 @@ resource "azurerm_subnet" "agw" {
 }
 
 resource "azurerm_subnet" "sql" {
+  depends_on = [azurerm_virtual_network.vnet]
+
   name                 = "subnet-sql"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
