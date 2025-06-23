8253fb0682ac2ac594e7724fee3e68780fce7268
devuser08 <devuser08@refabs365.com>
2025-06-23 10:46:20 +0000

    Updated main.tf

--- modules/SQLDatabase/main.tf
+++ modules/SQLDatabase/main.tf
@@ -3,9 +3,12 @@ resource "azurerm_mssql_server" "sql_server" {
   resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = "12.0"
-  administrator_login          = var.admin_username
-  administrator_login_password = var.admin_password
-  minimum_tls_version          = "1.2"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+
+  identity {
+    type = "SystemAssigned"
+  }
 
   tags = {
     environment = var.environment
@@ -13,15 +16,14 @@ resource "azurerm_mssql_server" "sql_server" {
 }
 
 resource "azurerm_mssql_database" "sql_db" {
-  name                = var.database_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  server_id           = azurerm_mssql_server.sql_server.id
-  sku_name            = var.sku_name
+  name               = var.sql_database_name
+  server_id          = azurerm_mssql_server.sql_server.id
+  sku_name           = "Basic"
+  collation          = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb        = 2
+  zone_redundant     = false
 
   tags = {
     environment = var.environment
   }
-
-  depends_on = [azurerm_mssql_server.sql_server]
 }
